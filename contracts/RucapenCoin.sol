cf470ac77dcbec9f573718fb6c25b75deb29051c
Bruno Mogetta <100445144+BMogetta@users.noreply.github.com>
2022-11-25 13:35:13 -0300

    Create RucapenCoin.sol

---
+++ contracts/RucapenCoin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract RucapenCoin is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+	/// @custom:oz-upgrades-unsafe-allow constructor
+
+	uint256 public maxSupply = 1_000_000_000 * 10 ** decimals();
+
+	constructor() {
+		_disableInitializers();
+	}
+
+	function initialize() public initializer {
+		__ERC20_init("RucapenCoin", "RCPN");
+		__Ownable_init();
+		__UUPSUpgradeable_init();
+
+		_mint(msg.sender, maxSupply);
+	}
+
+	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
